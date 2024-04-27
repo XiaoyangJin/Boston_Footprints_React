2d0f20239d1373caa00656d1ae26fd8e50c493bf
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-26 21:36:20 -0400

    Some prepare for create post

--- boston-footprints/src/components/CreatePost.jsx
+++ boston-footprints/src/components/CreatePost.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import '../css/CreatePost.css';
+import { useState } from 'react';
 
 function CreatePost() {
+    const [title, setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [media, setMedia] = useState([]);
+
     return (
-        <div>CreatePost</div>
+        <div className='create'>
+            <form className='create__form'>
+
+            </form>
+        </div>
     )
 }
 
