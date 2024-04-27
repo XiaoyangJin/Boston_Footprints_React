212901f20f61924b8bdfbd430e3b548e772e751e
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-26 22:53:31 -0400

    Create part form only update text

--- boston-footprints/src/components/CreatePost.jsx
+++ boston-footprints/src/components/CreatePost.jsx
@@ -27,9 +27,17 @@ function CreatePost() {
     }
 
     return (
-        <div className='create'>
+        <div className='create__content'>
+            <p className='title'>Create a New Post</p>
             <form className='create__form'>
+                <label className='create__form-lable' htmlFor='title'>Title: </label><br />
+                <input className='create__form-input' type='text' id='title' name='title' onChange={handleChange} /><br />
 
+                <label className='create__form-lable' htmlFor='summary'>Summary: </label><br />
+                <textarea rows="5" cols="50" id="summary" onChange={handleChange}>
+                </textarea><br />
+
+                <input className='create__form-button' type="submit" value="Submit" onClick={createPost} />
             </form>
         </div>
     )
